e7780928e54b5a32b1e828e11775b10acbc61a14
adamkong1995 <adamkong256@gmail.com>
2019-03-27 15:07:45 +0800

    manage task page

--- src/app.js
+++ src/app.js
@@ -1,18 +1,24 @@
 const app = require('./config')
-const sequelize = require('./database')
+//const sequelize = require('./database')
 
 app.get('/', (req, res) => {
-    res.render("task_update.ejs");
+    res.render("manage_task.ejs");
 })
+
+
+app.get('/task_update', (req, res) => {
+    res.render('task_update.ejs')
+})
+
 
 app.listen(5000, () => {
     console.log('Servers is up on port 5000');
-    sequelize
-        .authenticate()
-        .then(() => {
-            console.log('Connection has been established successfully.');
-        })
-        .catch(err => {
-            console.error('Unable to connect to the database:', err);
-        });
+    //sequelize
+    //    .authenticate()
+    //    .then(() => {
+    //        console.log('Connection has been established successfully.');
+    //    })
+    //    .catch(err => {
+    //        console.error('Unable to connect to the database:', err);
+    //    });
 })
--- src/config.js
+++ src/config.js
@@ -2,13 +2,13 @@ const path = require('path')
 const express = require('express')
 
 const app = express()
-
+console.log(__dirname)
 app.use(express.static(path.join(__dirname, '../public/stylesheets')));
 app.use(express.static(path.join(__dirname, '../public/javascript')));
 
 const publicDirectoryPath = [path.join(__dirname, '../public/templates'),
                              path.join(__dirname, '../public/templates/webpage/task_update'),
-                             path.join(__dirname, '../public/templates/webpage/editPage'),
+                             path.join(__dirname, '../public/templates/webpage/manage_task'),
                              path.join(__dirname, '../public/templates/webpage/updatePage'),
                              path.join(__dirname, '../public/templates/webpage/reportPage'),
                              path.join(__dirname, '../public/templates/webpage/administration')]
