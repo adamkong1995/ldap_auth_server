e3c77da27af5f55ccf3b9e0b67b19be7ee3aa428
adamkong1995 <adamkong256@gmail.com>
2019-03-28 17:43:16 +0800

    add basic route

--- src/app.js
+++ src/app.js
@@ -2,12 +2,37 @@ const app = require('./config')
 //const sequelize = require('./database')
 
 app.get('/', (req, res) => {
-    res.render("manage_task.ejs");
+    res.render("manage_task");
 })
 
 
-app.get('/task_update', (req, res) => {
-    res.render('task_update.ejs')
+app.get('/weekly_updates', (req, res) => {
+    res.render('weekly_updates')
+})
+
+
+app.get('/show_report', (req, res) => {
+    res.render('report')
+})
+
+
+app.get('/admin_group', (req, res) => {
+    res.render('admin_group')
+})
+
+
+app.get('/admin_subgroup', (req, res) => {
+    res.render('admin_subgroup')
+})
+
+
+app.get('/admin_freq', (req, res) => {
+    res.render('admin_freq')
+})
+
+
+app.get('/admin_updatetype', (req, res) => {
+    res.render('admin_updatetype')
 })
 
 
--- src/config.js
+++ src/config.js
@@ -7,11 +7,10 @@ app.use(express.static(path.join(__dirname, '../public/stylesheets')));
 app.use(express.static(path.join(__dirname, '../public/javascript')));
 
 const publicDirectoryPath = [path.join(__dirname, '../public/templates'),
-                             path.join(__dirname, '../public/templates/webpage/task_update'),
+                             path.join(__dirname, '../public/templates/webpage/weekly_updates'),
                              path.join(__dirname, '../public/templates/webpage/manage_task'),
-                             path.join(__dirname, '../public/templates/webpage/updatePage'),
-                             path.join(__dirname, '../public/templates/webpage/reportPage'),
-                             path.join(__dirname, '../public/templates/webpage/administration')]
+                             path.join(__dirname, '../public/templates/webpage/report'),
+                             path.join(__dirname, '../public/templates/webpage/admin')]
 
 app.set('views', publicDirectoryPath)
 app.set('view engine', 'ejs')
